0f251a5d0308c36b60771b04a229d9428363d5a5
Limesh-B <limesh075@gmail.com>
2024-12-03 11:32:09 +0530

    Added basic structure of classes

--- src/main/java/lk/limesh/ticketingapp/cli/Vendor.java
+++ src/main/java/lk/limesh/ticketingapp/cli/Vendor.java
@@ -1,4 +1,27 @@
 package lk.limesh.ticketingapp.cli;
+import java.math.BigDecimal;
 
-public class Vendor {
+public class Vendor implements Runnable {
+    private int totalTickets; // Tickets willing to sell
+    private int ticketReleaseRate; // Frequency of releasing
+    private TicketPool ticketPool; // Shared resource between Vendors and Customers
+
+    public Vendor(int totalTickets, int ticketReleaseRate, TicketPool ticketPool) {
+        this.totalTickets = totalTickets;
+        this.ticketReleaseRate = ticketReleaseRate;
+        this.ticketPool = ticketPool;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 1; i < totalTickets; i++) {
+            Ticket ticket = new Ticket(i,"Simple Event", new BigDecimal("1000"));
+            ticketPool.addTicket(ticket);
+            try {
+                Thread.sleep(ticketReleaseRate * 1000); // To calculate to MS
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 }
