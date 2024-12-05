bfbdc39d3bf29a2fb171395548c71eed25e580bf
Limesh-B <limesh075@gmail.com>
2024-12-05 17:33:43 +0530

    Made TicketingApp Controller and Service Classes

---
+++ src/main/java/lk/limesh/ticketingapp/TicketingAppController.java
@@ -0,0 +1,9 @@
+package lk.limesh.ticketingapp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/ticketingApp")
+public class TicketingAppController {
+}
