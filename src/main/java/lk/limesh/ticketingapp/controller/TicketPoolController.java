0119cae8a0977f4a0326c55b4c9f553100a2e914
Limesh-B <limesh075@gmail.com>
2024-12-06 14:30:53 +0530

    Update TicketPoolController
    There are some errors in the TicketPool class

--- src/main/java/lk/limesh/ticketingapp/controller/TicketPoolController.java
+++ src/main/java/lk/limesh/ticketingapp/controller/TicketPoolController.java
@@ -1,4 +1,73 @@
 package lk.limesh.ticketingapp.controller;
 
+import lk.limesh.ticketingapp.model.Ticket;
+import lk.limesh.ticketingapp.service.TicketPoolService;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.web.bind.annotation.*;
+import java.util.Queue;
+
+@RestController
+@RequestMapping("/api/v1/ticket-pool")  // Endpoint for ticket pool
 public class TicketPoolController {
+
+    private static final Logger logger = LoggerFactory.getLogger(TicketPoolController.class);
+    private TicketPoolService ticketPoolService;
+
+    /**
+     * Constructor for dependency injection of TicketPoolService.
+     * @param ticketPoolService: instance/object of TicketPoolService.
+     */
+    public TicketPoolController(TicketPoolService ticketPoolService) {
+        this.ticketPoolService = ticketPoolService;
+    }
+
+    /**
+     * Endpoint to add tickets to the ticket pool. Synchronized to handle concurrent access.
+     * @param ticket: Ticket object that will be created after deserializing the JSON data from the request body
+     *              using @RequestBody.
+     */
+    @PostMapping("/add-ticket")
+    public synchronized void addTicket(@RequestBody Ticket ticket) {
+        while (ticketPoolService.getTickets().size() >= ticketPoolService.getMaxTicketCapacity()) {
+            try {
+                wait();
+                logger.info("Waiting for space in ticket pool...");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e.getMessage());
+            }
+        }
+        ticketPoolService.addTicket(ticket);
+        notifyAll();  // Notify all the waiting threads
+        logger.info("Ticket added by: " + Thread.currentThread().getName() + " Added ticket: " + ticket);
+    }
+
+    /**
+     * Endpoint to buy a ticket from the ticket pool. Synchronized to handle concurrent access.
+     * @return A ticket object.
+     */
+    @GetMapping("/buy-ticket")
+    public synchronized Ticket buyTicket() {
+        while (ticketPoolService.getTickets().isEmpty()) {
+            try {
+                wait();
+                logger.info("Waiting for tickets to be added...");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e.getMessage());
+            }
+        }
+        logger.info("Ticket bought by: " + Thread.currentThread().getName() +
+                " Bought ticket: " + ticketPoolService.getTickets().peek());
+        notifyAll();  // Notify all the waiting threads
+        return ticketPoolService.getTicket();
+    }
+
+    /**
+     * Endpoint to retrieve all the tickets from the ticket pool.
+     * @return A queue of all tickets currently in the ticket pool.
+     */
+    @GetMapping("/get-all-tickets")
+    public Queue<Ticket> getAllTicket() {
+        return ticketPoolService.getTickets();
+    }
 }
--- src/main/java/lk/limesh/ticketingapp/service/TicketPoolService.java
+++ src/main/java/lk/limesh/ticketingapp/service/TicketPoolService.java
@@ -1,10 +1,41 @@
 package lk.limesh.ticketingapp.service;
 
 import lk.limesh.ticketingapp.model.Ticket;
+import lombok.Getter;
+import org.springframework.stereotype.Service;
 
+import java.util.LinkedList;
 import java.util.Queue;
 
+@Service
+@Getter
 public class TicketPoolService {
 
-    private Queue<Ticket> tickets;
+    private Queue<Ticket> tickets;  // Queue to store the Tickets
+    private int maxTicketCapacity;  // Maximum ticket capacity that can be in the TicketPool
+
+    /**
+     * Default constructor for TicketPoolService class.
+     * Initializes the tickets Queue and sets the maxTicketCapacity.
+     */
+    public TicketPoolService() {
+        tickets = new LinkedList<Ticket>();
+        maxTicketCapacity = 10;
+    }
+
+    /**
+     * Adds a ticket to the ticket pool. (Used by vendor)
+     * @param ticket: The ticket to be added to the pool.
+     */
+    public void addTicket(Ticket ticket) {
+        tickets.add(ticket);
+    }
+
+    /**
+     * Returns the ticket from the ticket pool. (Used by customer)
+     * @return the next ticket in the ticket pool or null if the ticket pool is empty.
+     */
+    public Ticket getTicket() {
+        return tickets.poll();
+    }
 }
