9d561cd3793d9c055cec692c8897c675b84a96d1
Limesh-B <limesh075@gmail.com>
2024-12-06 20:22:21 +0530

    Update CustomerController.java

--- src/main/java/lk/limesh/ticketingapp/controller/CustomerController.java
+++ src/main/java/lk/limesh/ticketingapp/controller/CustomerController.java
@@ -1,4 +1,9 @@
 package lk.limesh.ticketingapp.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/custmer")
 public class CustomerController {
 }
