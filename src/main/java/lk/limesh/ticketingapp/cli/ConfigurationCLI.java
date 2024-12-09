de1c70975091cb7db116e5cf069993f5213fea1a
Limesh-B <limesh075@gmail.com>
2024-12-09 16:31:21 +0530

    Create ConfigurationCLI.java

---
+++ src/main/java/lk/limesh/ticketingapp/cli/ConfigurationCLI.java
@@ -0,0 +1,12 @@
+package lk.limesh.ticketingapp.cli;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Scanner;
+
+public class ConfigurationCLI {
+
+    private final Logger logger = LoggerFactory.getLogger(ConfigurationCLI.class);
+    Scanner user
+}
